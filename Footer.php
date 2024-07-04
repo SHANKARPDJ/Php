2108548eb06d6a97da41d726639e12d13e57ee8a
SHANKARPDJ <40641611+SHANKARPDJ@users.noreply.github.com>
2024-07-04 16:18:10 +0530

    Create Footer.php

---
+++ Footer.php
@@ -0,0 +1,6 @@
+           
+           <br><br>
+<div class="footer">
+<p>Powered by || <a href="https://skmaga.site" target="_blank">SkMaga.Site</a> || <a href="./sitemap.xml">SiteMap</a> || 
+(3) online </p>
+ </div></footer></body></html>
