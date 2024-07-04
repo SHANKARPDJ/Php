4297cd39e0260cb3265656ba9116d1a319533b9a
SHANKARPDJ <40641611+SHANKARPDJ@users.noreply.github.com>
2024-07-05 00:04:31 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,7 +1,15 @@
 <tr><td><center><div class="block"><div class="cardk"><a class="fileName" href="/play.php?A=https://pagalmovies.tv.in/download/17969/server_2&B=new Hollywood movie&C=https://i.postimg.cc/MGSh3F3p/pushpa-the-rule-part-2.jpg">
 <table><tr>
 <th><img class="absmiddle" src="https://i.postimg.cc/MGSh3F3p/pushpa-the-rule-part-2.jpg" width="110" height="150" alt=""></th>
-<td><b style="color: ;font-style: oblique;">Pushpa 3</b><br>
+<td><b style="color: ;font-style: oblique;">R R R 3</b><br>
+<b style="color: ;font-style: oblique;"><i class="fa fa-calendar" aria-hidden="true"></i> 10-03-2025 </b><br>
+<b style="color: ;font-style: oblique;"> <i class="fa fa-folder-open" aria-hidden="true"></i> Hindi Movie</b>
+</tr></table></div></div></center></tr>
+
+<tr><td><center><div class="block"><div class="cardk"><a class="fileName" href="/play.php?A=https://pagalmovies.tv.in/download/17969/server_2&B=new Hollywood movie&C=https://i.postimg.cc/MGSh3F3p/pushpa-the-rule-part-2.jpg">
+<table><tr>
+<th><img class="absmiddle" src="https://i.postimg.cc/MGSh3F3p/pushpa-the-rule-part-2.jpg" width="110" height="150" alt=""></th>
+<td><b style="color: ;font-style: oblique;">K G F 3</b><br>
 <b style="color: ;font-style: oblique;"><i class="fa fa-calendar" aria-hidden="true"></i> 10-03-2025 </b><br>
 <b style="color: ;font-style: oblique;"> <i class="fa fa-folder-open" aria-hidden="true"></i> Hindi Movie</b>
 </tr></table></div></div></center></tr>
