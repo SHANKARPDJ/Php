b5f03e539ff26a56f708552566ed2c4f778e74a5
SHANKARPDJ <40641611+SHANKARPDJ@users.noreply.github.com>
2024-07-05 01:05:27 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -6,21 +6,3 @@
 <td><b style="color: ;font-style: oblique;">A Quiet Place: Day One (2024) Hindi Dubbed Hollywood Movie</b><br>
 <b style="color: ;font-style: oblique;"> <i class="fa fa-folder-open" aria-hidden="true"></i> Audio Hindi HD</b>
 </tr></table></div></div></center></tr>
-
-	
-<tr><td><center><div class="block"><div class="cardk">
-<a class="fileName" href="/play.php?A=https://pagalmovies.tv.in/download/17971/server_1&B=A Quiet Place: Day One (2024) Hindi Dubbed Hollywood Movie&C=https://pagalmovies.tv.in/files/images/A_Quiet_Place:_Day_One_(2024)_Hindi_Dubbed_Hollywood_Movie.jpg&D=Audio Hindi HD">
-<table><tr>
-<th><img src="https://pagalmovies.tv.in/files/images/A_Quiet_Place:_Day_One_(2024)_Hindi_Dubbed_Hollywood_Movie.jpg" width="110" height="150" alt=""></th>
-<td><b style="color: ;font-style: oblique;">A Quiet Place: Day One (2024) Hindi Dubbed Hollywood Movie</b><br>
-<b style="color: ;font-style: oblique;"> <i class="fa fa-folder-open" aria-hidden="true"></i> Audio Hindi HD</b>
-</tr></table></div></div></center></tr>
-
-  
-<tr><td><center><div class="block"><div class="cardk"><a class="fileName" href="/play.php?A=https://pagalmovies.tv.in/download/17969/server_2&B=new Hollywood movie&C=https://i.postimg.cc/MGSh3F3p/pushpa-the-rule-part-2.jpg">
-<table><tr>
-<th><img class="absmiddle" src="https://i.postimg.cc/MGSh3F3p/pushpa-the-rule-part-2.jpg" width="110" height="150" alt=""></th>
-<td><b style="color: ;font-style: oblique;">Pushpa 2</b><br>
-<b style="color: ;font-style: oblique;"><i class="fa fa-calendar" aria-hidden="true"></i> 10-03-2025 </b><br>
-<b style="color: ;font-style: oblique;"> <i class="fa fa-folder-open" aria-hidden="true"></i> Hindi Movie</b>
-</tr></table></div></div></center></tr>
