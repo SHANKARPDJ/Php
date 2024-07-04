2753ee71aca0718fa12dca0f8b614c769f75a5c3
SHANKARPDJ <40641611+SHANKARPDJ@users.noreply.github.com>
2024-07-04 23:43:26 +0530

    Update Footer.php

--- Footer.php
+++ Footer.php
@@ -1,5 +1,4 @@
-           
-           <br><br>
+<br><br>
 <div class="footer">
 <p>Powered by || <a href="http://skmaga.site" target="_blank">SkMaga.Site</a> || <a href="./sitemap.xml">SiteMap</a> || 
 (3) online </p>
