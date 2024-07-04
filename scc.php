934a9782470911536bc53f260b97c56a4fa1b105
SHANKARPDJ <40641611+SHANKARPDJ@users.noreply.github.com>
2024-07-04 14:47:30 +0530

    scc.php

---
+++ scc.php
@@ -0,0 +1,81 @@
+<style>
+
+.header {
+  padding: 5px;
+  text-align: center;
+background-image: linear-gradient(to top right, #6601BF, #FC08E4);  color: white;
+  text-align: left;
+  font-weight: bold;
+} 
+
+.footer {
+  padding: 10px;
+  text-align: center;
+  
+background-image: linear-gradient(to top right, #6601BF, #FC08E4);  color: white;
+  font-weight: bold;
+} 
+.footer a{ color:#fff;}
+.header a{ color:#fff;}
+
+
+h3 {
+  color: white;
+background-image: linear-gradient(to top right, #6601BF, #FC08E4);  padding: 6px 2%; 
+  text-align: left;
+  margin: 10px;
+  font-size: 16px;
+  border-radius: 0px;
+  width: 87%;
+}
+
+.block {
+  transition: 0.3s;
+  width: 90%; 
+ text-align: left;
+ padding: 0px 0px;
+}
+
+.block a div div {
+            vertical-align: middle;
+            display: table-cell
+        }
+        
+        .block a {
+            display: block;
+            padding: 5px
+        }
+        .block a img {
+            padding: 0;
+            margin-right: 5px
+            
+            
+        }
+
+
+.block img {
+            
+            padding: 1px;
+            border-radius: 5px;
+            background:#000;
+        }
+
+
+	* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0px;
+  font-family: 'segoe ui';
+}
+a {
+            text-decoration: none;
+            color: #000;
+            
+        }
+        a:hover {
+            text-decoration: none;
+        }
+
+	</style>
