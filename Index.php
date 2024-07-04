51dfd69057ac1c9beac59c3e5ef9fac90c3b4da9
SHANKARPDJ <40641611+SHANKARPDJ@users.noreply.github.com>
2024-07-04 18:56:40 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,17 +1,9 @@
 <center><h3>☆ Latest Updates</h3></center>
            
-<center><div class="block"><div class="cardk"><a class="fileName" href="download/Insta+Millionaire+1194.html">
+<center><div class="block"><div class="cardk"><a class="fileName" href="/?play=https://cgjnf.com/moviess/Ganapath.mp4">
 <table><tr>
 <td><img class="absmiddle" src="https://i.postimg.cc/MGSh3F3p/pushpa-the-rule-part-2.jpg" width="110" height="150" alt=""></td>
 <td><b style="color: ;font-style: oblique;">Pushpa 2</b><br>
 <b style="color: ;font-style: oblique;"><i class="fa fa-calendar" aria-hidden="true"></i> 10-03-2025 </b><br>
 <b style="color: ;font-style: oblique;"> <i class="fa fa-folder-open" aria-hidden="true"></i> Hindi Movie</b>
-</tr></table></div></div></div></div></center>
-
-<center><div class="block"><div class="cardk"><a class="fileName" href="download/Insta+Millionaire+1194.html">
-<table><tr>
-<td><img class="absmiddle" src="https://i.postimg.cc/MGSh3F3p/pushpa-the-rule-part-2.jpg" width="110" height="150" alt=""></td>
-<td><b style="color: ;font-style: oblique;">Pushpa 2</b><br>
-<b style="color: ;font-style: oblique;"><i class="fa fa-calendar" aria-hidden="true"></i> 10-03-2025 </b><br>
-<b style="color: ;font-style: oblique;"> <i class="fa fa-folder-open" aria-hidden="true"></i> Hindi Movie</b>
-</tr></table></div></div></div></div></center>
+</tr></table></div></div></center>
