836a082f1ad23d6be296f0d34891e5ff628ff12a
SHANKARPDJ <40641611+SHANKARPDJ@users.noreply.github.com>
2024-07-04 17:54:31 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -2,10 +2,10 @@
            
 <center><div class="block"><div class="cardk"><a class="fileName" href="download/Insta+Millionaire+1194.html">
 <table><tr>
-<td><img class="absmiddle" src="img/Insta Millionaire 1194" width="100" height="100" alt=""></td>
-<td><b style="color: ;font-style: oblique;">Insta Millionaire 1194</b><br>
+<td><img class="absmiddle" src="https://i.postimg.cc/MGSh3F3p/pushpa-the-rule-part-2.jpg" width="100" height="130" alt=""></td>
+<td><b style="color: ;font-style: oblique;">Pushpa 2</b><br>
 <b style="color: ;font-style: oblique;"><i class="fa fa-calendar" aria-hidden="true"></i> 10-03-2025 </b><br>
-<b style="color: ;font-style: oblique;"> <i class="fa fa-folder-open" aria-hidden="true"></i> Insta Millionaire</b>
+<b style="color: ;font-style: oblique;"> <i class="fa fa-folder-open" aria-hidden="true"></i> Hindi Movie</b>
 </tr></table></div></div></div></div>
 </center>
 
