67166ae352c222dcb5828b4762b86b3c2f450624
SHANKARPDJ <40641611+SHANKARPDJ@users.noreply.github.com>
2024-07-04 22:48:30 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,6 +1,6 @@
 <center><h3>☆ Latest Updates</h3></center>
            
-<center><div class="block"><div class="cardk"><a class="fileName" href="/play.php?A=https://pagalmovies.tv.in/download/17969/server_2&B=new Hollywood movie">
+<center><div class="block"><div class="cardk"><a class="fileName" href="/play.php?A=https://pagalmovies.tv.in/download/17969/server_2&B=new Hollywood movie&C=https://i.postimg.cc/MGSh3F3p/pushpa-the-rule-part-2.jpg">
 <table><tr>
 <td><img class="absmiddle" src="https://i.postimg.cc/MGSh3F3p/pushpa-the-rule-part-2.jpg" width="110" height="150" alt=""></td>
 <td><b style="color: ;font-style: oblique;">Pushpa 2</b><br>
