b950cd4a9cfc6f39c6bda55f38b98b2cfbe79cf3
SHANKARPDJ <40641611+SHANKARPDJ@users.noreply.github.com>
2024-07-04 16:15:15 +0530

    Create Header.php

---
+++ Header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>My HopHub</title>
+
+    <meta name="author" content="Codeconvey" />
+    <!-- Navbar CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <!--Only for demo purpose - no need to add.-->
+    <link rel="stylesheet" href="css/demo.css" />
+	
+</head>
+<body>
+		
+<a href="/"><div class="header"><img src="https://i.postimg.cc/bJJV1QSq/20240303-211701.png" alt="logo" width="300" height="60">
+</div></a><br>
