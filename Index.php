39551172be5ee995d821fc8262ad62d74f779282
SHANKARPDJ <40641611+SHANKARPDJ@users.noreply.github.com>
2024-07-05 01:06:26 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -2,7 +2,7 @@
 <tr><td><center><div class="block"><div class="cardk">
 <a class="fileName" href="/play.php?A=https://pagalmovies.tv.in/download/17971/server_1&B=A Quiet Place: Day One (2024) Hindi Dubbed Hollywood Movie&C=https://pagalmovies.tv.in/files/images/A_Quiet_Place:_Day_One_(2024)_Hindi_Dubbed_Hollywood_Movie.jpg&D=Audio Hindi HD">
 <table><tr>
-<th><img src="https://pagalmovies.tv.in/files/images/A_Quiet_Place:_Day_One_(2024)_Hindi_Dubbed_Hollywood_Movie.jpg" width="110" height="150" alt=""></th>
+<th><img src="https://pagalmovies.tv.in/files/images/A_Quiet_Place:_Day_One_(2024)_Hindi_Dubbed_Hollywood_Movie.jpg" width="110" height="110" alt=""></th>
 <td><b style="color: ;font-style: oblique;">A Quiet Place: Day One (2024) Hindi Dubbed Hollywood Movie</b><br>
 <b style="color: ;font-style: oblique;"> <i class="fa fa-folder-open" aria-hidden="true"></i> Audio Hindi HD</b>
 </tr></table></div></div></center></tr>
