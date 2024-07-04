f4cd5ec9ba44f327f6c6a9503196f83f2ef76838
SHANKARPDJ <40641611+SHANKARPDJ@users.noreply.github.com>
2024-07-05 01:12:43 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -5,7 +5,7 @@
 <table><tr>
 <th><img src="https://pagalmovies.tv.in/files/images/A_Quiet_Place:_Day_One_(2024)_Hindi_Dubbed_Hollywood_Movie.jpg" width="110" height="110" alt=""></th>
 <td><b style="color: ;font-style: oblique;">A Quiet Place: Day One (2024) Hindi Dubbed Hollywood Movie</b><br>
-<b style="color: ;font-style: oblique;"> <i class="fa fa-folder-open" aria-hidden="true"></i> Audio Hindi HD</b>
+<b style="color: ;font-style: oblique;color;red;"> <i class="fa fa-folder-open" aria-hidden="true"></i> Audio Hindi HD</b>
 </tr></table></div></div></center></tr>
 
 	
@@ -14,6 +14,6 @@
 <table><tr>
 <th><img src="https://pagalmovies.tv.in/files/images/Beverly_Hills_Cop:_Axel_F_(2024)_Hindi_Dubbed_Hollywood_Movie.jpg" width="110" height="110" alt=""></th>
 <td><b style="color: ;font-style: oblique;">Beverly Hills Cop: Axel F (2024) Hindi Dubbed Hollywood Movie</b><br>
-<b style="color: ;font-style: oblique;"> <i class="fa fa-folder-open" aria-hidden="true"></i> Audio Hindi HD</b>
+<b style="color: ;font-style: oblique;color:blue;"> <i class="fa fa-folder-open" aria-hidden="true"></i> Audio Hindi HD</b>
 </tr></table></div></div></center></tr>
 
