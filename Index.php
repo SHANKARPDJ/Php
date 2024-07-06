1448af5a208c3446ad239b4d23c87137e939468c
SHANKARPDJ <40641611+SHANKARPDJ@users.noreply.github.com>
2024-07-07 03:01:02 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,11 +1,1 @@
 
-
-<tr><td><center><div class="block"><div class="cardk">
-<a href="/play.php?A=http://103.145.232.246/Data/movies/Bollywood/2024/Kalki%202898%20AD%20(2024)/Kalki%202898%20AD.mp4&B=Kalki 2898 AD (2024)&C=http://103.145.232.246//Admin/main/images/tt12735488/poster/original//zNE88KjSX2nDSWjdsNqeIFGAXo8.jpg&D=VIP QUALITY MOVIE">
-<table><tr>
-<th><img src="http://103.145.232.246//Admin/main/images/tt12735488/poster/original//zNE88KjSX2nDSWjdsNqeIFGAXo8.jpg" width="110" height="110" alt=""></th>
-<td><b style="color: ;font-style: oblique;">Kalki 2898 AD (2024)</b><br>
-<b style="color:red"><i class="fa fa-folder-open" aria-hidden="true"></i> VIP QUALITY MOVIE</b>
-</tr></table></div></div></center></tr>
-
-
