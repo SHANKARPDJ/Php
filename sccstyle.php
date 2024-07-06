4aa95c0f411583162581ec9ce75f30684fc57607
SHANKARPDJ <40641611+SHANKARPDJ@users.noreply.github.com>
2024-07-07 05:07:17 +0530

    Update sccstyle.php

--- sccstyle.php
+++ sccstyle.php
@@ -1,3 +1,9 @@
+<?php
+$firstNames = ['red', 'DarkMagenta', 'orange', 'green', 'DarkBlue'];
+$randomFirstName = $firstNames[array_rand($firstNames)];
+$randomName = $randomFirstName ;
+?>
+
 <style>
 
 :root {--bg-color: #3F51B5; }
@@ -51,4 +57,44 @@ a {
 	</style>
 
 
+<style>
+body{ margin:0px;
+ }
 
+b { float: left ;background: <?php echo $randomName ; ?> ;border-radius: 5px;padding: 2px 10px;color:#ffffff;}
+k {font-weight: bold; color:<?php echo $randomName ; ?>;}
+#myInput{
+ 
+  width: 87%;
+  font-size: 16px;
+  padding: 12px 20px 12px 10px;
+  border: 2px solid #3F51B5;
+  margin-bottom: 12px;
+}
+
+#myTable {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 87%;
+  font-size: 18px;
+}
+
+
+ #myTable td {
+  text-align: left;
+  border-bottom: 1px solid #ddd;
+  padding: 10px 16px;
+}
+
+#myTable th { 
+width: 10%;
+border-bottom: 1px solid #ddd;
+text-align: left;
+}
+
+#myTable tr.header, #myTable tr:hover {
+  background-color: #f1f1f1;
+
+}
+
+</style>
