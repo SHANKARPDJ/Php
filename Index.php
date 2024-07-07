7beb6d1871461ee717793a3cd9e083e8f423a940
SHANKARPDJ <40641611+SHANKARPDJ@users.noreply.github.com>
2024-07-07 05:58:51 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -2,13 +2,13 @@
 
 <tr><th><img src="https://khesari2.in/siteuploads/thumb/c/20554_4.jpg"width="128" height="128"></th><td>
 <a href="/play.php?A=https://khesari2.in/siteuploads/files/sfd70/34877/Vidai%20Kaise%20Kari%20Judai%20Kaise%20Sahi%20Dil%20Me%20Tuhi%20Baat%20Dil%20Ke%20Ham%20Kahi%20Pawan%20Singh%20(Full%20Vaibretion%20Tahalka%20Mix%20Dj%20Song)%20Dj%20Vimalesh%20Raja(Khesari2.IN).mp3&B=Vidai Kaise Kari Judai Kaise Sahi Dil Me Tuhi Baat Dil Ke Ham Kahi Pawan Singh (Full Vaibretion Tahalka Mix Dj Song) Dj Vimalesh Raja&C=https://khesari2.in/siteuploads/thumb/c/20554_4.jpg&D=Singer: - Dj Vimalesh RajaPawan SinghPawan Singh Dj Remix Mp3 Songs">
-<k>Vidai Kaise Kari Judai Kaise Sahi Dil Me Tuhi Baat Dil Ke Ham Kahi Pawan Singh (Full Vaibretion )</k><br><br><b>Singer: - Dj Vimalesh </b> </a></td></tr>
+<k>Vidai Kaise Kari Judai Kaise Sahi Dil Me Tuhi Baat Dil Ke Ham Kahi Pawan Singh </k><br><br><b>Singer: - Dj Vimalesh </b> </a></td></tr>
 
 
 
 
 <tr><th><img src="https://khesari2.in/siteuploads/thumb/sft70/34855_3.jpg"width="128" height="128"></th><td>
 <a href="/play.php?A=https://khesari2.in/siteuploads/files/sfd70/34855/Dhire%20Bola%20Ae%20Koyaliya%20Dhire%20Bola%20Ho%20Maiya%20Mori%20Jaag%20Jaihe%20Ho%20(Pawan%20Singh)%20Dj%20Remix%20Gana%202023%20-%20Dj%20Vimalesh%20Raja(Khesari2.IN).mp3&B=Dhire Bola Ae Koyaliya Dhire Bola Ho Maiya Mori Jaag Jaihe Ho (Pawan Singh)&C=https://khesari2.in/siteuploads/thumb/sft70/34855_3.jpg&D=Dj Vimalesh Raja">
-<k>Dhire Bola Ae Koyaliya Dhire Bola Ho Maiya Mori Jaag Jaihe Ho (Pawan Singh)</k><br><br><b>Dj Vimalesh Raja</b> </a></td></tr>
+<k>Dhire Bola Ae Koyaliya Dhire Bola Ho Maiya Mori Jaag Jaihe Ho </k><br><br><b>Dj Vimalesh Raja</b> </a></td></tr>
 
 
