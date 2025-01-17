accadb08faecfe023cac2b40bb361908649fd76f
SHANKARPDJ <40641611+SHANKARPDJ@users.noreply.github.com>
2025-01-17 17:02:54 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -2,3 +2,6 @@
 <a href="/play.php?A=https://khesari2.in/siteuploads/files/sfd70/34877/Vidai%20Kaise%20Kari%20Judai%20Kaise%20Sahi%20Dil%20Me%20Tuhi%20Baat%20Dil%20Ke%20Ham%20Kahi%20Pawan%20Singh%20(Full%20Vaibretion%20Tahalka%20Mix%20Dj%20Song)%20Dj%20Vimalesh%20Raja(Khesari2.IN).mp3&B=Vidai Kaise Kari Judai Kaise Sahi Dil Me Tuhi Baat Dil Ke Ham Kahi Pawan Singh (Full Vaibretion Tahalka Mix Dj Song) Dj Vimalesh Raja&C=https://khesari2.in/siteuploads/thumb/c/20554_4.jpg&D=Singer: - Dj Vimalesh RajaPawan SinghPawan Singh Dj Remix Mp3 Songs">
 <k>  SONG DJ REMIX  </k><br><br><b>Singer: - Dj Deepak kumar </b> </a></td></tr>
 
+
+
+
